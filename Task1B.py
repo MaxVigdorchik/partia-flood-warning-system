c9fa90d41157a07a00523a48f128cb380a0fe1a8
MaxVigdorchik <maxv008@gmail.com>
2017-01-19 21:58:19 +0000

    Fixed up some of the syntax in Task1B

--- Task1B.py
+++ Task1B.py
@@ -3,23 +3,24 @@ from floodsystem.stationdata import build_station_list
 
 
 def run():
-    """Requirements for Task 1A"""
+    """Requirements for Task 1B"""
 
     # Build list of stations
     stations = build_station_list()
+    # Find distances of all stations from Cambridge
     distance_list = stations_by_distance(
         stations, (52.2053, 0.1218))
 
-    namelist = []
-    for pair in distance_list:
-        station, distance = pair
-        namelist.append((station.name, distance))
+    namelist = [(station.name, station.town, distance)
+                for (station, distance) in distance_list]
 
     print(namelist[:10])
+    print("\n")
+    print(namelist[-10:])
 
 
 if __name__ == "__main__":
-    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    print("*** Task 1B: CUED Part IA Flood Warning System *** \n")
 
     # Run Task1B
     run()
